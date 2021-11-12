eb00fbac99f3db0e4c960b9dc71f1d7a23b97311
Rodionov Taras <moonrtv@ya.ru>
2021-11-12 17:03:03 +0300

    Внёс правки после ревью

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,2 @@
-export const initialState = {
-  size: {id: 'size30', name: '30', cost: 200},
-  dough: {},
-  sauce: {},
-  cheese: [],
-  vegetables: [],
-  meat: [],
-};
-
 export const MIN = 1000;
 export const MAX = 9999;
